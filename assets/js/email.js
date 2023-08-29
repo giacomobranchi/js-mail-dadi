326ab7b581fd963bcc11ef225ae40c2879a22d4e
giacomobranchi <branchigiacomo28@gmail.com>
2023-08-29 17:16:48 +0200

    refactoring

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,18 +1,40 @@
-const validEmail = [];
+const validEmail = ["ex1@mail.com", "ex2@mail.com", "ex3@mail.com", "ex4@mail.com",];
 
 let userEmail = document.querySelector(".email");
 
 let verifyBtn = document.querySelector(".verify");
 
-verifyBtn.addEventListener("click", function () {
 
-    if (validEmail.includes(`${userEmail}`)) {
-        const html = `L'indirizzo è valido!`;
-        verifyBtn.insertAdjacentHTML("afterend", html);
+verifyBtn.addEventListener("click", function () {
+    let check = false;
+
+    console.log(validEmail.length)
+
+    for (let i = 0; i < validEmail.length; i++) {
+
+        console.log(validEmail[i])
+
+        console.log(userEmail.value)
+
+        if (validEmail[i] === userEmail.value) {
+            check = true
+        }
+
+
     }
 
-    else {
+    if (check) {
+        const html = (`<div>L'indirizzo è valido!</div>`);
+        verifyBtn.insertAdjacentHTML("afterend", html);
+    } else {
         const html = (`<div>L'indirizzo non è valido!</div>`);
         verifyBtn.insertAdjacentHTML("afterend", html);
     }
 });
+
+
+
+/*else {
+            const html = (`<div>L'indirizzo non è valido!</div>`);
+            verifyBtn.insertAdjacentHTML("afterend", html);
+        }*/
